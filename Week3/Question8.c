1ca0d101aebe307d64d0210a4cf8fb0c04c0aaaf
Chaman Rawat <chamanrawat.dev@gmail.com>
2021-11-03 19:03:10 +0530

    Changed the logic

--- Week3/Question8.c
+++ Week3/Question8.c
@@ -4,33 +4,18 @@
 int main(int argc, char const *argv[])
 {
     int T, G;
-    scanf("%d%d", &T, &G);
-    int games[G][3];
-    for (int i = 0; i < G; i++)
-        scanf("%d%d%d", &games[i][0], &games[i][1], &games[i][2]);
-
-    int result[G];
-    for (int i = 0; i < G; i++)
+    scanf("%d", &T);
+    while (T--)
     {
-        int game[games[i][1]];
-        result[i] = 0;
-
-        // Set coins face to default before playing
-        for (int j = 0; j < games[i][1]; j++)
-            game[j] = (games[i][0] == 1) ? 1 : 2;
-
-        // Flip the coins
-        for (int j = 0; j < games[i][1]; j++)
-            for (int k = 0; k <= j; k++)
-                game[k] = (game[k] == 1) ? 2 : 1;
-
-        // All turns over now, count the result
-        for (int j = 0; j < games[i][1]; j++)
-            if (games[i][2] == game[j])
-                result[i]++;
+        scanf("%d", &G);
+        int games[G][3], result[G];
+        for (int i = 0; i < G; i++)
+        {
+            scanf("%d%d%d", &games[i][0], &games[i][1], &games[i][2]);
+            result[i] = (games[i][1] + (games[i][0] == games[i][2] ? 0 : 1)) / 2;
+        }
+        for (int i = 0; i < G; i++)
+            printf("%d\n", result[i]);
     }
-
-    for (int i = 0; i < G; i++)
-        printf("%d\n", result[i]);
     return 0;
 }
