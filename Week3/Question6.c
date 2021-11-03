b08b1c8bfc344c5d23211b664ce8515367de0865
Chaman Rawat <chamanrawat.dev@gmail.com>
2021-11-03 11:31:38 +0530

    Changed Logic in Question6 - Used array hashing technique to precalculate all elements frequency

--- Week3/Question6.c
+++ Week3/Question6.c
@@ -1,17 +1,22 @@
 // Count Total Number of occurrences of a given element in an array.
 #include <stdio.h>
 
+const int hash_size = 1e+7; // 1e+7 = 10000000
+int hash[hash_size]; // No need to intialise with 0
+
 int main(int argc, char const *argv[])
 {
     int size;
     scanf("%d", &size);
 
-    int arr[size], element, occurrence = 0;
-    for (int i = 0; i < size; i++) scanf("%d", &arr[i]);
+    int arr[size], element;
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+        hash[arr[i]]++;
+    }
     scanf("%d", &element);
 
-    for (int i = 0; i < size; i++) if (arr[i] == element) occurrence++;
-
-    printf("%d\n", occurrence);
+    printf("%d\n", hash[element]);
     return 0;
 }
