26282537734a90322a5cfe36608c5359980d1832
Chaman Rawat <chamanrawat.dev@gmail.com>
2021-11-09 22:15:06 +0530

    Modified Week4/Question2 - Added code for Addon Part

--- Week4/Question2.c
+++ Week4/Question2.c
@@ -20,9 +20,27 @@ int main()
 
     printf("Output: \n");
     for (int i = 0; i < hash_size; i++)
-    {
         if (hash[i])
             printf("%d - %d times\n", i, hash[i]);
-    }
+
+    // ----------
+    // Addon Part
+    // ----------
+    int num;
+    printf("\n\t(Check Existence of a number)\nEnter Number : ");
+    scanf("%d", &num);
+
+    // Linear Search method
+    int is_exist = 0;
+    for (int i = 0; i < size; i++)
+        if (arr[i] == num)
+            is_exist++;
+
+    printf("Using Linear Search: Element ");
+    printf((is_exist) ? "Exist\n" : "Not Exist\n");
+
+    // Array Hashing mathod
+    printf("Using Array Hashing: Element ");
+    printf((hash[num]) ? "Exist\n" : "Not Exist\n");
     return 0;
 }
