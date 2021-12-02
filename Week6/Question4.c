eb89bf24950c769ad0460a6f410739864c977ffe
chaman-rawat <chamanrawat.dev@gmail.com>
2021-12-02 22:30:28 +0530

    Added Question4.c

---
+++ Week6/Question4.c
@@ -0,0 +1,62 @@
+// Roman to Integer
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int roman_to_int(char *);
+int count_digits(int);
+
+int main()
+{
+    char roman[10];
+    printf("Input: ");
+    scanf("%s", roman);
+
+    int integer = roman_to_int(roman);
+    printf("Output: %d\n", integer);
+    return 0;
+}
+
+int roman_to_int(char *roman)
+{
+    const char symbol[] = "IVXLCDM";
+    const int value[] = {1, 5, 10, 50, 100, 500, 1000};
+
+    const int len = strlen(roman);
+    const int symbol_len = strlen(symbol);
+    int integer = 0;
+
+    for (int i = 0; i < len; i++)
+        for (int j = 0; j < symbol_len; j++)
+            if (roman[i] == symbol[j]) // Symbol Identified
+            {
+                if (roman[i + 1] == symbol[j + 1]) // Check special cases
+                {
+                    int exponent = count_digits(value[j + 1] - 1) - 1;
+                    int subtract = pow(10, exponent);
+                    integer += value[j + 1] - subtract;
+                    i++; // skip next letter
+                }
+                else if (roman[i + 1] == symbol[j + 2]) // Check special cases
+                {
+                    int exponent = count_digits(value[j + 2] - 1) - 1;
+                    int subtract = pow(10, exponent);
+                    integer += value[j + 2] - subtract;
+                    i++; // skip next letter
+                }
+                else // No special case
+                    integer += value[j];
+
+                break;
+            }
+
+    return integer;
+}
+
+int count_digits(int number)
+{
+    int digit = 1;
+    while (number /= 10)
+        digit++;
+    return digit;
+}
